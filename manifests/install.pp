0785a6058d4fa262a8723ac4f8d6d1e99c98b71c
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-10-30 16:59:43 -0700

    working service running

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,15 @@ class ci_eye::install inherits ci_eye {
 
   notify { "Installing CI-eye..." : }
 
-  # TODO handle existing file, overwrite?  version upgrade?
-  file { "/tmp/ci-eye.jar" : # TODO gotta be some place better to put this file
+  include java
+
+  # TODO:
+  # - gotta be some place better to put this file
+  # - handle existing file, overwrite?  version upgrade?
+  # - need to set mode / owner / group ... maybe use source_permissions?
+
+  file { "/tmp/ci-eye.jar" : # TODO
     ensure => file,
     source => "puppet:///modules/ci_eye/ci-eye-0.4.0.jar",
-    # TODO probably need to set mode / owner / group ... maybe use source_permissions
   }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,20 @@
 class ci_eye::service inherits ci_eye {
 
-  # TODO ...
-  # - not actually a service
-  # - java dep
-  # - how do we stop it?
 
   notify { "Running CI-eye..." : }
 
-  # TODO PORT needs to be configurable
-  # TODO doesn't return ... um ...
-  exec { "java -jar /tmp/ci-eye.jar PORT" :
-    # let's try to make puppet find java on the $PATH
-    path => $path,
+  # TODO ...
+  # - java dep
+  # - how do we stop it?
+  # - CI eye .jar file path should be a var, perhaps even come from config?
+  # - notify something like 'CI eye is now running at URL'
+
+  $port = 55555 # TODO should come from config
+
+  java_service_wrapper::service{ 'ci_eye':
+    wrapper_mainclass  => 'WrapperJarApp',
+    wrapper_classpath  => ['/usr/local/lib/wrapper.jar', '/tmp/ci-eye.jar'],
+    wrapper_parameter  => [' /tmp/ci-eye.jar', $port]
   }
 
   notify { "CI-eye started successfully." : }
