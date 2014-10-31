995f0b9b97b079ee041575f376418ae805afd769
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-10-31 16:32:22 -0700

    recover file resource for install dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,13 @@ class ci_eye::install inherits ci_eye {
   # Ensure that java is installed
   include java
 
+  file { "${install_dir}" :
+    ensure  => directory,
+    owner   => 0,
+    group   => 0,
+    mode    => '0755',
+    recurse => true,
+  } ~>
   file { "${install_dir}/${jar_name}" :
     ensure => file,
     source => "puppet:///modules/ci_eye/${jar_name}",
